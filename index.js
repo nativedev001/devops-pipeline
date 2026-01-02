8440a8c2823fa84405f9b6fa229c48c55dfec07f
nativedev001 <nativexdeveloper@gmail.com>
2026-01-02 16:25:32 +0500

    new change form dev taeam

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 console.log('Hello, World!');
 console.log('I am learning the ci cd pipeline.');
 console.log('This is my first commit to the repository.');
+
+console.log("here is new changes form the development")
